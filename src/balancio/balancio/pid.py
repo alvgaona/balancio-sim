8df1ca0d351f5a5f811a6da648181f42b9cb3982
Alvaro <alvgaona@gmail.com>
2024-07-20 20:03:18 -0300

    feat: declare new parameters

--- src/balancio/balancio/pid.py
+++ src/balancio/balancio/pid.py
@@ -12,6 +12,13 @@ class BalancioPID(Node):
         self.get_logger().set_level(rclpy.logging.LoggingSeverity.DEBUG)
 
         self.declare_parameter(
+            name="set_point",
+            descriptor=ParameterDescriptor(
+                description="Controller set-point", type=ParameterType.PARAMETER_DOUBLE
+            ),
+            value=0.0,
+        )
+        self.declare_parameter(
             name="kp",
             descriptor=ParameterDescriptor(
                 description="Proportional gain", type=ParameterType.PARAMETER_DOUBLE
@@ -29,19 +36,34 @@ class BalancioPID(Node):
                 description="Derivative gain", type=ParameterType.PARAMETER_DOUBLE
             ),
         )
+        self.declare_parameter(
+            name="sum_constraint",
+            descriptor=ParameterDescriptor(
+                description="Integral error constraint",
+                type=ParameterType.PARAMETER_DOUBLE,
+            ),
+            value=1.0,
+        )
 
-        self.dt = 0.01
+        self.dt = 0.05
         self.subscription = self.create_subscription(Imu, "/imu", self.imu_callback, 1)
         self.publisher = self.create_publisher(Twist, "/cmd_vel", 10)
         self.timer = self.create_timer(self.dt, self.update)
 
         self.theta = 0
-        self.desired_theta = 0
+        self.set_point = 0
         self.error_sum = 0
         self.error_prev = 0
+
+        self.set_point = (
+            self.get_parameter("set_point").get_parameter_value().double_value
+        )
         self.kp = self.get_parameter("kp").get_parameter_value().double_value
         self.ki = self.get_parameter("ki").get_parameter_value().double_value
         self.kd = self.get_parameter("kd").get_parameter_value().double_value
+        self.sum_constraint = (
+            self.get_parameter("sum_constraint").get_parameter_value().double_value
+        )
 
     def imu_callback(self, msg: Imu):
         self.get_logger().debug("Received IMU data:")
@@ -63,24 +85,26 @@ class BalancioPID(Node):
         self.get_logger().debug(f" Tilt angle: {np.degrees(self.theta)}")
 
     def update(self):
-        error = self.theta - self.desired_theta
+        error = self.theta - self.set_point
 
         self.get_logger().info(f"Error: {error}")
 
         self.error_sum += error
-        self.error_sum = np.clip(self.error_sum, -1, 1)
+        self.error_sum = np.clip(
+            self.error_sum, -self.sum_constraint, self.sum_constraint
+        )
 
         u = (
             self.kp * error
             + self.ki * self.error_sum * self.dt
-            + self.kd * (self.error - self.error_prev) / self.dt
+            + self.kd * (error - self.error_prev) / self.dt
         )
 
         self.error_prev = error
 
         twist_msg = Twist()
         twist_msg.linear.x = (
-            u * 0.2 if self.theta < np.pi / 4 and self.theta > -np.pi / 4 else 0.0
+            u if self.theta < np.pi / 4 and self.theta > -np.pi / 4 else 0.0
         )
 
         self.publisher.publish(twist_msg)
