7fbe55b0da9d350fa1fc9efde088a548e87d6fbc
Alvaro <alvgaona@gmail.com>
2024-08-01 11:04:44 -0300

    remove cartpole package

--- src/cartpole/cartpole/controllers/controller.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Controller(ABC):
-    @abstractmethod
-    def compute(self, x: float):
-        pass
--- src/cartpole/test/test_mpc.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.mark.skip(reason="No test has been created yet")
-@pytest.mark.copyright
-@pytest.mark.linter
-def test_pid():
-    assert 1 == 2
