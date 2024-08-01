923723569a55c8097e063eb194fdd9025e78ac7a
Alvaro <alvgaona@gmail.com>
2024-08-01 10:56:17 -0300

    add find-packages

--- src/cartpole/setup.py
+++ src/cartpole/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = "cartpole"
 
 setup(
     name=package_name,
     version="0.0.1",
-    packages=[package_name],
+    packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
